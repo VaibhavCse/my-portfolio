9c9c8f5fb40da6d6b6b2733472970434caa3d3ad
Vaibhav Chaudhary <vaibhav77324@gmail.com>
2024-10-04 23:10:22 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,19 +8,6 @@ const Contact = () => {
     message: ''
   });
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const mailtoLink = `mailto:vaibhav77324@gmail.com?subject=Message from ${formData.name}&body=${formData.message}%0D%0A%0D%0AFrom: ${formData.email}`;
-    window.location.href = mailtoLink;
-  };
-
   return (
     <section id="contact" className="contact-section">
        <h2>Let's Connect</h2>
@@ -34,40 +21,6 @@ const Contact = () => {
         <li><a href="https://discord.com/channels/723908317705142382/723908318543741010">Discord</a></li>
       </ul>
 
-      {/* <form onSubmit={handleSubmit} className="contact-form">
-        <h3>Any queries?</h3>
-        <label htmlFor="name">Name</label>
-        <input 
-          type="text" 
-          id="name" 
-          name="name" 
-          value={formData.name} 
-          onChange={handleChange} 
-          required 
-        />
-
-        <label htmlFor="email">Email</label>
-        <input 
-          type="email" 
-          id="email" 
-          name="email" 
-          value={formData.email} 
-          onChange={handleChange} 
-          required 
-        />
-
-        <label htmlFor="message">Message</label>
-        <textarea 
-          id="message" 
-          name="message" 
-          rows="5" 
-          value={formData.message} 
-          onChange={handleChange} 
-          required 
-        />
-
-        <button type="submit">Send</button>
-      </form> */}
     </section>
   );
 };
